610070b91e31c51694e085dece3f363a1c406055
Louis Tsang <kawingt2@gmail.com>
2022-12-25 22:36:13 +0800

    fix image display issue

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const card = document.querySelector(".card");
+const rightCard = document.querySelector(".right-card");
 
 card.addEventListener("click", function () {
   card.classList.toggle("open");
+  rightCard.classList.toggle("closed");
 });
