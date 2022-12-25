ab0764adceded2e6fbe02ad830c31e30e53e3067
Louis Tsang <kawingt2@gmail.com>
2022-12-25 22:46:46 +0800

    打包整理

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const card = document.querySelector(".card");
 const rightCard = document.querySelector(".right-card");
 
+// 負責控制卡片的開合
 card.addEventListener("click", function () {
   card.classList.toggle("open");
   rightCard.classList.toggle("closed");
